76357d7a1d78c8b91d3814e7b31704db450aa013
Sabrina Furtado <sabrina.s.furtado@gmail.com>
2024-04-11 17:05:45 -0300

    Construindo a lógica do Componente Login

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="flex justify-center bg-fuchsia-800 text-white">
       <div className="container flex flex-col items-center py-4">
-        <p className='text-xl font-light'>Blog pessoal Sabrina Furtado | Copyright: </p>
+        <p className='text-xl font-light'>Blog pessoal Sabrina Furtado | Copyright: 2024 </p>
         <p className='text-lg'>Acesse minhas redes sociais</p>
         <div className='flex gap-2'>
           <a href="https://www.linkedin.com/in/sabrina-dos-santos-furtado-67374a185/" target="_blank" rel="noopener noreferrer">
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,6 @@
-//import React from 'react';
+import React from 'react';
+import homeLogo from "C:/Users/sabri/OneDrive/Desktop/blog-pessoal-client/sa.jpeg"
 import './Home.css';
-import homeLogo from 'C:/Users/sabri/OneDrive/Desktop/blog-pessoal-client/sa.jpeg'
-
 
 function Home() {
     return (
@@ -9,12 +8,12 @@ function Home() {
         <div className="bg-fuchsia-500 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-ligth'>Seja bem vinde!</h2>
+              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
               <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
   
               <div className="flex justify-around gap-4">
               
-                <button className='rounded bg-fuchsia-900 text-white py-2 px-4'>Ver postagens</button>
+                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
               </div>
             </div>
   
@@ -24,7 +23,7 @@ function Home() {
             </div>
           </div>
         </div>
-      
+        
       </>
     );
 }
